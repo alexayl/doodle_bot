48cc63b4d48b965593ebcb9f603814adacde450b
tconcan <tconcan@purdue.edu>
2025-10-02 16:15:17 -0400

    img2graph updates

--- pathfinding/img2graph.py
+++ pathfinding/img2graph.py
@@ -16,8 +16,7 @@ def save_image(fig, img_name, directory, extension=".png"):
         img_name (str): Name of the image file without extension
         directory (str): Directory where to save the image
         extension (str): File extension (e.g., '.png', '.jpg', '.pdf')
-        dpi (int): Resolution in dots per inch
-    """    
+    """
     # Create directory if it doesn't exist
     os.makedirs(directory, exist_ok=True)
     
@@ -78,7 +77,7 @@ def load_image(img_name, directory="png/", extension=".png", debug=False, save=F
 
     return img
 
-def scale_to_canvas(img, canvas_size=(1000//3, 1460//3), padding_percentage=0.1, debug=False, save=False):
+def scale_to_canvas(img, canvas_size=(1000, 1460), padding_percentage=0.1, debug=False, save=False):
     """
         Scale an image to fit within a specified canvas size while maintaining 
         aspect ratio.
@@ -119,16 +118,20 @@ def scale_to_canvas(img, canvas_size=(1000//3, 1460//3), padding_percentage=0.1,
     # Resize image maintaining aspect ratio
     img_resized = img.resize((scaled_width, scaled_height), Image.Resampling.LANCZOS)
     
-    # Detect background color from image corners
-    corners = [
-        img.getpixel((0, 0)),
-        img.getpixel((img.width-1, 0)),
-        img.getpixel((0, img.height-1)),
-        img.getpixel((img.width-1, img.height-1))
-    ]
+    # Get median value of all edge pixels
+    img_array = np.array(img)
+    edge_pixels = []
 
-    # Use the most common corner color as background
-    bg_color = max(set(corners), key=corners.count)
+    # Top and bottom edges
+    edge_pixels.extend(img_array[0, :])  # Top row
+    edge_pixels.extend(img_array[-1, :])  # Bottom row
+
+    # Left and right edges (excluding corners already counted)
+    edge_pixels.extend(img_array[1:-1, 0])  # Left column
+    edge_pixels.extend(img_array[1:-1, -1])  # Right column
+
+    # Use median of all edge pixels as background
+    bg_color = int(np.median(edge_pixels))
 
     # Create new image with canvas size and paste resized image centered
     canvas = Image.new('L', canvas_size, bg_color)
@@ -175,72 +178,6 @@ def edge_map(img, debug=False, save=False):
     print("Edge map created successfully")
 
     return edge_map
-
-def majority_filter(img):
-        
-    kernel = np.array([[1, 0.5, 1],
-                       [0.5, 3, 0.5],
-                       [1, 0.5, 1]])
-    
-    neighbor_count = convolve(img.astype(int), kernel, mode='nearest')
-    filtered_img = (neighbor_count >= 4).astype(int)
-    
-    return filtered_img
-
-# def threshold_selection(img, debug=False, smoothing=0, save=False):
-    
-#     img_array = np.array(img)
-#     # thresholds = [round(num, 2) for num in list(np.logspace(-2, -0.3, 9))]
-#     thresholds = list(np.linspace(0.1, 0.9, 9))
-#     thresholded_imgs = []
-#     selected_index = [None]
-
-#     fig, axes = plt.subplots(3, 3, figsize=(12, 8))
-#     axes = axes.flatten()
-    
-#     def on_click(event):
-#         if event.inaxes in axes:
-#             index = list(axes).index(event.inaxes)
-#             selected_index[0] = index
-#             plt.close(fig)
-    
-#     for i, threshold in enumerate(thresholds):
-#         binary_img = 1 - (img_array > threshold * 255).astype(int)
-#         for _ in range(smoothing):
-#             binary_img = majority_filter(binary_img)
-#         thresholded_imgs.append(binary_img)
-#         axes[i].imshow(binary_img, cmap='gray')
-#         axes[i].set_title(f"{i + 1}: threshold = {threshold}" if debug else f"{i + 1}")
-#         axes[i].axis('off')
-    
-#     fig.canvas.mpl_connect('button_press_event', on_click)
-#     plt.suptitle("Click on desired threshold image")
-#     plt.tight_layout()
-#     plt.show()
-    
-#     if selected_index[0] is not None:
-#         selected_img = thresholded_imgs[selected_index[0]]
-#         print("Threshold selected successfully")
-#         plt.close('all')
-#         plt.title("chosen mappping")
-#         plt.imshow(selected_img, cmap='gray')
-#         plt.axis('off')
-#         plt.show()
-#         if save:
-#             save_image(plt, "threshold_selection_" + img_name, "outputs/threshold_selection/", extension=".png")
-#         plt.close()
-#         return selected_img
-#     else:
-#         print("Threshold selected unsuccessfully")
-#         plt.close('all')
-#         plt.title("chosen mappping")
-#         plt.imshow(selected_img, cmap='gray')
-#         plt.axis('off')
-#         plt.show()
-#         if save:
-#             save_image(plt, "threshold_selection_" + img_name, "outputs/threshold_selection/", extension=".png")
-#         plt.close()
-#         return thresholded_imgs[5]
     
 def node_map(img, debug=False, save=False):
 
@@ -252,11 +189,12 @@ def node_map(img, debug=False, save=False):
     for y in range(img.shape[0]):
         for x in range(img.shape[1]):
             if (img[y,x] == 0):
-                nodes[(x, cols - y)] = (x, cols - y)
+                nodes[(x, rows - y)] = (x, rows - y)
     graph.add_nodes_from(nodes.keys())
 
+    # debug output
     if debug:
-        print("nodes: ", len(nodes))
+        print("nodes:             ", graph.number_of_nodes())
 
     # connect neighboring nodes
     for (x, y) in nodes:
@@ -266,23 +204,111 @@ def node_map(img, debug=False, save=False):
                     continue
                 p = (x + dx, y + dy)
                 if p in nodes:
-                    dist = np.sqrt(dx ** 2 + dy ** 2)
-                    graph.add_edge((x, y), p)
+                    graph.add_edge((x, y), p, weight=0)
 
+    # remove staircase nodes
+    for (x, y) in nodes:
+        graph_nodes = graph.nodes
+        n = (x, y - 1) in graph_nodes
+        w = (x - 1, y) in graph_nodes
+        s = (x, y + 1) in graph_nodes
+        e = (x + 1, y) in graph_nodes
+        if n and w and not (s or e) or \
+           n and e and not (s or w) or \
+           s and w and not (n or e) or \
+           s and e and not (n or w) or \
+           n + e + s + w > 2:
+            graph.remove_node((x, y))
+    for (x, y) in graph.nodes:
+        while graph.degree((x, y)) >= 3:
+            neighbor = list(graph.neighbors((x, y)))[0]
+            graph.remove_edge((x, y), neighbor)
+
+    # merge disjoint sections
+    while len(list(nx.connected_components(graph))) > 1:
+        component_id = 0
+        endpoints = []
+        min_size = 5
+        for disjoint in list(nx.connected_components(graph)):
+            if len(disjoint) < min_size:
+                graph.remove_nodes_from(disjoint)
+            else:
+                component_id += 1
+                end_not_fount = True
+                for node in disjoint:
+                    if graph.degree(node) == 1:
+                        end_not_fount = False
+                        graph.nodes[node]['color'] = 'red'
+                        graph.nodes[node]['endpoint'] = True
+                        graph.nodes[node]['componentID'] = component_id
+                        endpoints.append(node)
+                    # elif ('color' in graph.nodes[node]):
+                    #     graph.nodes[node]['color'] = 'black'
+                    #     graph.nodes[node]['endpoint'] = False
+                    #     graph.nodes[node]['componentID'] = component_id
+                if end_not_fount:
+
+                    # Add a few possible endpoints
+                    # for i in range(0, len(disjoint), min_size):
+                    #     node = list(disjoint)[i]
+                    #     graph.nodes[node]['color'] = 'red'
+                    #     graph.nodes[node]['endpoint'] = True
+                    #     graph.nodes[node]['componentID'] = component_id
+                    #     endpoints.append(node)
+
+                    # Add one endpoint
+                    node = list(disjoint)[0]
+                    graph.nodes[node]['color'] = 'red'
+                    graph.nodes[node]['endpoint'] = True
+                    graph.nodes[node]['componentID'] = component_id
+                    endpoints.append(node)
+
+        # Collect all potential edges with distances
+        smallest_num_edges = 1
+        for node1 in endpoints:
+            node1_edges = []
+            for node2 in endpoints:
+                if node1 != node2 and graph.nodes[node1]['componentID'] != graph.nodes[node2]['componentID']:
+                    distance = np.hypot(node1[0] - node2[0], node1[1] - node2[1])
+                    node1_edges.append((node1, node2, distance))
+            
+            # Sort by distance and take smallest_num_edges smallest for this endpoint
+            node1_edges.sort(key=lambda x: x[2])
+            smallest_for_node1 = node1_edges[:smallest_num_edges]
+            
+            # Add smallest_num_edges for this endpoint
+            for node1, node2, distance in smallest_for_node1:
+                graph.add_edge(node1, node2, weight=distance)
+                graph[node1][node2]['color'] = 'red'
+    
+    # debug output
     if debug:
-        print("edges: ", len(graph.edges))
+        print("nodes (optimized): ", graph.number_of_nodes())  
+        reduction_percentage = (len(nodes) / graph.number_of_nodes() - 1) * 100
+        print(f"  reduced by {reduction_percentage:.1f}%")
+        print("edges:             ", graph.number_of_edges())
 
-    # Set figure size to match image aspect ratio
-    aspect_ratio = cols / rows
-    plt.figure(figsize=(5 * aspect_ratio, 5))
-    nx.draw(graph, nodes, node_size=3)
-    plt.gca().set_aspect('equal')
-    plt.xlim(0, cols)
-    plt.ylim(0, rows)
-    plt.axis('on')
-    plt.show()
-    plt.close()
-
+    # visualize node map
+    if debug or save:
+        aspect_ratio = cols / rows
+        fig, ax = plt.subplots(figsize=(5 * aspect_ratio, 5))
+        ax.set_title("node map of " + img_name_ext)
+        pos = {node: node for node in graph.nodes()}
+        nx.draw(graph, pos, 
+                node_size=5,
+                node_color=[graph.nodes[node].get('color', 'black') for node in graph.nodes()], 
+                edge_color=[graph.edges[edge].get('color', 'black') for edge in graph.edges()],
+                ax=ax)
+        ax.set_aspect('equal')
+        ax.set_xlim(0, cols)
+        ax.set_ylim(0, rows)
+        ax.axis('on')
+        if debug:
+            plt.show()
+        if save:
+            save_image(fig, "node_map_" + img_name, "outputs/node_map/", extension=".png")
+        plt.close(fig)
+        
     return
 
 def img2graph(img_name, debug, save):
@@ -290,12 +316,21 @@ def img2graph(img_name, debug, save):
     img = load_image(img_name, debug=debug[0], save=save[0])
     img_scaled = scale_to_canvas(img, debug=debug[1], save=save[1])
     img_edge = edge_map(img_scaled, debug=debug[2], save=save[2])
-    # img_pix_map = threshold_selection(img_edge, debug=debug[3], save=save[3])
     img_graph = node_map(img_edge, debug=debug[4], save=save[4])
+    return img_graph
 
 if __name__ == "__main__":
 
-    debug_all = False
+    parser = argparse.ArgumentParser(description="Convert an image to a graph")
+    parser.add_argument("img_name", help="image name in pathfinding png/")
+    parser.add_argument("--debug", action="store_true", help="display debug information")
+    parser.add_argument("--save", action="store_true", help="save debug information")
+    args = parser.parse_args()
+
+    img_name = args.img_name
+    img_name_ext = img_name + ".png"
+
+    debug_all = args.debug
     display_array = [False,   # load_image
                      False,   # scale_to_canvas
                      False,   # edge_map
@@ -303,12 +338,12 @@ if __name__ == "__main__":
                      True     # node_map
     ]
 
-    save_all = False
+    save_all = args.save
     save_array = [False,   # load_image
                   False,   # scale_to_canvas
                   False,   # edge_map
-                  True,   # threshold selection
-                  True    # node_map
+                  False,   # threshold selection
+                  False    # node_map
     ]
     
     if debug_all == True:
@@ -317,11 +352,10 @@ if __name__ == "__main__":
     if save_all == True:
         save_array = [True, True, True, True, True]
 
-    parser = argparse.ArgumentParser(description="Convert an image to a graph")
-    parser.add_argument("img_name", help="image name in pathfinding png/")
-    args = parser.parse_args()
-
-    img_name = args.img_name
-    img_name_ext = img_name + ".png"
-
-    img2graph(args.img_name, display_array, save_array)
+    if args.img_name == 'all':
+        for file in os.listdir("png/"):
+            if file.endswith(".png"):
+                img_name = file[:-4]
+                img2graph(img_name, display_array, save_array)
+    else:
+        img2graph(args.img_name, display_array, save_array)
