1fb8b058457c91aae53f419fb019a5e74bfcf9be
Alex Aylward <alexaylward@outlook.com>
2025-11-23 12:32:44 -0500

    fw: new body dropped

--- firmware/include/config.h
+++ firmware/include/config.h
@@ -7,10 +7,10 @@
 #define DEBUG_INTERPOLATE
 
 #define WHEEL_DIAMETER          (47.30)
-#define WHEELBASE               (127.5)
+#define WHEELBASE               (154.0)
 
 #define STEPPER_CTRL_FREQ       (5.0f)     // control frequency (Hz)
 
 #define WHEEL_RADIUS            (WHEEL_DIAMETER / 2)
 #define DOODLEBOT_RADIUS        (WHEELBASE / 2)   // distance from center of doodlebot to wheel
-#define MAX_LINEAR_VELOCITY     (25.0f) // maximum linear velocity (mm/s)
+#define MAX_LINEAR_VELOCITY     (80.0f) // maximum linear velocity (mm/s)
